d2e81caa282a5056d169a9015565bde32d9b38ab
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-28 15:00:50 +0300

    Compontset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Try from './comopents/Try';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
     <div className="App">
      <h1>Conflict Redressal System</h1>
+     <SignUp/>
     </div>
   );
 }
---
+++ src/comopents/Try.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Try() {
+  return (
+    <div>Try Component</div>
+  )
+}
+
+export default Try
