673568cc11c78d8304c6d4bfe6c0fa1c9b58762a
Hamisi-Esq <brivahamisi@gmail.com>
2022-10-20 23:25:11 +0300

    Code refactoring

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -106,6 +106,7 @@ export default function Login() {
                 </Link>
               </Typography>
             )}
+            
           </ContentStyle>
         </Container>
       </RootStyle>
